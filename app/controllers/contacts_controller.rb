c4a40d4fb7dbae447f64c61502afb5cbb40c88af
Anne D'Mura <jianna.prefect@gmail.com>
2015-06-22 04:15:02 +0000

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,16 +6,21 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
+      name = params[:contact][:name]
+      email = param[:contact][:email]
+      body = params[:contact][:comments]
+      
+      ContactMailer.contact_email(name, email, body).deliver
       flash[:info] = "Message sent."
       redirect_to new_contact_path
     else
       flash[:danger] = "Error occured, message has not been sent."
       redirect_to new_contact_path
+    end
   end
-end
   
-private
-  def contact_params
-    params.require(:contact).permit(:name, :email, :comments)
-  end
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
